d85c328a33e564ed48a49abdc3f21b95f46eb562
Mostafa Gamal <mostafa.gamal8086@gmail.com>
2017-08-23 23:13:19 +0200

    Layers have become generic for all subprojects

--- gp/layers/utils.py
+++ gp/layers/utils.py
@@ -61,3 +61,47 @@ def get_deconv_filter(f_shape, l2_strength):
 
     init = tf.constant_initializer(value=weights, dtype=tf.float32)
     return variable_with_weight_decay(weights.shape, init, l2_strength)
+
+
+def noise_and_argmax(logits):
+    # Add noise then take the argmax
+    noise = tf.random_uniform(tf.shape(logits))
+    return tf.argmax(logits - tf.log(-tf.log(noise)), 1)
+
+
+def openai_entropy(logits):
+    # Entropy proposed by OpenAI in their A2C baseline
+    a0 = logits - tf.reduce_max(logits, 1, keep_dims=True)
+    ea0 = tf.exp(a0)
+    z0 = tf.reduce_sum(ea0, 1, keep_dims=True)
+    p0 = ea0 / z0
+    return tf.reduce_sum(p0 * (tf.log(z0) - a0), 1)
+
+
+def softmax_entropy(p0):
+    # Normal information theory entropy by Shannon
+    return - tf.reduce_sum(p0 * tf.log(p0 + 1e-6), axis=1)
+
+
+def mse(pred, target):
+    # Mean-squared error
+    return tf.square(pred - target) / 2.
+
+
+def ortho_init(scale=1.0):
+    def _ortho_init(shape, dtype, partition_info=None):
+        # lasagne ortho init for tf
+        shape = tuple(shape)
+        if len(shape) == 2:
+            flat_shape = shape
+        elif len(shape) == 4:  # assumes NHWC
+            flat_shape = (np.prod(shape[:-1]), shape[-1])
+        else:
+            raise NotImplementedError
+        a = np.random.normal(0.0, 1.0, flat_shape)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        q = u if u.shape == flat_shape else v  # pick the one with the correct shape
+        q = q.reshape(shape)
+        return (scale * q[:shape[0], :shape[1]]).astype(np.float32)
+
+    return _ortho_init
