7791a74da2943a8a86a034f07bc13acd14f4b81e
Mostafa Gamal <mostafa.gamal8086@gmail.com>
2017-08-28 16:11:31 +0000

    Inference function added

--- gp/a2c/a2c.py
+++ gp/a2c/a2c.py
@@ -6,11 +6,12 @@ from gp.a2c.models.model import Model
 from gp.a2c.train.train import Trainer
 from gp.configs.a2c_config import A2CConfig
 
+
 class A2C:
     def __init__(self):
         tf.reset_default_graph()
 
-        #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
         config = tf.ConfigProto(allow_soft_placement=True,
                                 intra_op_parallelism_threads=A2CConfig.num_envs,
                                 inter_op_parallelism_threads=A2CConfig.num_envs)
@@ -47,14 +48,27 @@ class A2C:
             env = self.__make_all_environments(num_envs=1, env_class=GymEnv, env_name=A2CConfig.env_name, seed=A2CConfig.env_seed)
             if A2CConfig.record_video_every != -1:
                 env.monitor(is_monitor=True, is_train=False, experiment_dir=A2CConfig.experiment_dir,
-                                 record_video_every=A2CConfig.record_video_every)
+                            record_video_every=A2CConfig.record_video_every)
             else:
                 env.monitor(is_monitor=True, is_train=False, experiment_dir=A2CConfig.experiment_dir,
-                                 record_video_every=20)
+                            record_video_every=20)
             self.trainer.test(total_timesteps=total_timesteps, env=env)
         except KeyboardInterrupt:
             print('Error occured..')
             self.env.close()
+
+    def infer(self, observation):
+        """Used for inference.
+        :param observation: (tf.tensor) having the shape (None,img_height,img_width,num_classes*num_stack)
+        :return action after noise and argmax
+        :return value function of the state
+        """
+        states = self.model.step_policy.initial_state
+        dones = []
+        # states and dones are for LSTM, leave them for now!
+        action, value, states = self.model.step_policy.step(observation, states, dones)
+        return action, value
+
 
     # The reason behind this design pattern is to pass the function handler when required after serialization.
     def __env_maker(self, env_class, env_name, i, seed):
@@ -62,6 +76,7 @@ class A2C:
             return env_class(env_name, i, seed)
 
         return __make_env
+
 
     def __make_all_environments(self, num_envs=4, env_class=GymEnv, env_name="SpaceInvaders", seed=42):
         set_all_global_seeds(seed)
@@ -73,4 +88,4 @@ class A2C:
 if __name__ == '__main__':
     a2c = A2C()
     a2c.train()
-    #a2c.test(total_timesteps=10000000)
+    # a2c.test(total_timesteps=10000000)
