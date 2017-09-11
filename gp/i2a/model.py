cdd9c7b673db757bec310948b80233a41afeb057
mrgemy95 <mahmoud.gemy18@gmail.com>
2017-09-12 00:50:46 +0200

    integration

--- gp/i2a/model.py
+++ gp/i2a/model.py
@@ -26,8 +26,9 @@ class I2a:
         return output[:-1], output[-1]
 
     def build_model(self):
-        rollout = Rollout(self.input, self.initial_lstm_state, self.config)
-        rollout_encoder = RolloutsEncoder(rollout.imagined_observations, rollout.imagined_rewards, self.config)
-        a2c_cnn = A2CCNN(self.input)
-        model_free_feature_map = a2c_cnn.cnn_output()
-        policy, v = self.actor_critic(rollout_encoder.rollout_encoding, model_free_feature_map)
+        with tf.name_scope('i2a_model'):
+            rollout = Rollout(self.input, self.initial_lstm_state, self.config)
+            rollout_encoder = RolloutsEncoder(rollout.imagined_observations, rollout.imagined_rewards, self.config)
+            a2c_cnn = A2CCNN(self.input)
+            model_free_feature_map = a2c_cnn.cnn_output()
+            policy, v = self.actor_critic(rollout_encoder.rollout_encoding, model_free_feature_map)
