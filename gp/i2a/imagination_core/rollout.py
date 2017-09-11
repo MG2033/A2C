53ae5f77551380885d97bdf137c5eb89610c07db
mrgemy95 <mahmoud.gemy18@gmail.com>
2017-09-11 22:18:57 +0200

    integration

--- gp/i2a/imagination_core/rollout.py
+++ gp/i2a/imagination_core/rollout.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 from gp.i2a.imagination_core.rollout_policy import RolloutPolicy
 from gp.res.model import RESModel
 import numpy as np
-
+from gp.configs.res_config import ResConfig
 
 class Rollout:
     def __init__(self, observation, lstm_state, config):
         self.config = config
         self.observation = observation
-        res_model = RESModel()
+        res_config=ResConfig()
+        res_model = RESModel(res_config)
         self.res_templete = res_model.template()
         self.policy_templete = RolloutPolicy.policy_template('rollout_policy', config.actions_num)
         self.initial_lstm_state = lstm_state
@@ -29,18 +30,19 @@ class Rollout:
                               shape=[self.config.actions_num] * 2)
         multiples = tf.constant([self.config.actions_num, 1, 1, 1], tf.int32, shape=[4])
         observations = tf.tile(self.observation, multiples)
-
+        lstm_states = self.initial_lstm_state
         for i in range(self.config.rollouts_steps):
             observations, rewards, lstm_states = rollout_res_network(observations, actions,
-                                                                     self.initial_lstm_state)
+                                                                     lstm_states)
 
             observations_unwrap.append(observations)
             rewards_unwrap.append(rewards)
             actions_prob = rollout_policy_network(observations)
             actions = tf.arg_max(actions_prob)
 
-            observations_unwrap = tf.stack(observations_unwrap)
-            rewards_unwrap = tf.stack(rewards_unwrap)
+        observations_unwrap = tf.stack(observations_unwrap)
+        rewards_unwrap = tf.stack(rewards_unwrap)
 
-            self.imagined_observations = tf.transpose(observations_unwrap, [1, 0, 2, 3, 4])
-            self.imagined_rewards = tf.transpose(rewards_unwrap, [1, 0, 2])
+        self.imagined_observations = tf.transpose(observations_unwrap, [1, 0, 2, 3, 4])
+        self.imagined_rewards = tf.transpose(rewards_unwrap, [1, 0, 2])
+        return self.imagined_observations,self.imagined_rewards,self.rollout_actions_prob
